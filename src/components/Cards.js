794db3b51eee1893568c6eb1ebd8e88a060db2f1
Naman <namanb454@gmail.com>
2023-04-28 23:41:35 +0530

    8th commit

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -5,12 +5,12 @@ function Cards() {
     return (
         <div>
             <section class="text-gray-600 body-font">
-                <div class="container flex px-52 py-24">
-                    {/* <div class="flex flex-wrap px- -m-4 mx-auto"> */}
-                    <div class="p- mx-auto" id='card'>
-                        <div class="h-96 bg-opacity-75 px-8 pt-16 pb-24 overflow-hidden relative" id='cards1'>
+                <div class="container flex sm:px-52 py-24">
+                    <div class="flex flex-wrap px- -m-4 mx-auto">
+                    <div class="sm:w-96 sm:mx-5" id='card'>
+                        <div class="h-96 bg-opacity-75 px-8 pt-10 pb-24 overflow-hidden relative" id='cards1'>
                             <h2 class="tracking-widest text-xs title-font text-black font-semibold mb-5">Stem</h2>
-                            <h1 class="title-font sm:text-2xl text-xl text-center font-bold text-black mb-3">How Upbringing
+                            <h1 class="title-font sm:text-2xl text-xl text-center font-bold text-black mb-16">How Upbringing
                                 changed the parent’s
                                 approach</h1>
                             <p class="leading-relaxed text-center text-black mb-3">explore how a person's upbringing can impact their approach to parenting. We'll discuss how parents' own experiences as children can influence their beliefs, attitudes, and behaviors as parents. </p>
@@ -18,22 +18,22 @@ function Cards() {
                     </div>
 
 
-                    <div class="p- pt-24 mx-auto" id='card'>
-                        <div class="h-96 bg-opacity-75 px-8 pt-16 pb-24 overflow-hidden relative" id='cards1'>
+                    <div class="sm:w-96 pt-24 sm:mx-5" id='card'>
+                        <div class="h-96 bg-opacity-75 px-8 pt-10 pb-24 overflow-hidden relative" id='cards1'>
                             <h2 class="tracking-widest text-xs title-font text-black font-semibold mb-5">Tips</h2>
-                            <h1 class="title-font sm:text-2xl text-center text-xl font-bold text-black mb-3">Enhance the Social
+                            <h1 class="title-font sm:text-2xl text-center text-xl font-bold text-black mb-16">Enhance the Social
                                 Interaction in Kids </h1>
                             <p class="leading-relaxed text-center text-black mb-3">explore how a person's upbringing can impact their approach to parenting. We'll discuss how parents' own experiences as children can influence their beliefs, attitudes, and behaviors as parents. </p>
                         </div>
-                        {/* </div> */}
+                        </div>
                     </div>
                 </div>
-                <div class="container flex px-52 py-24">
-                    {/* <div class="flex flex-wrap px- -m-4 mx-auto"> */}
-                    <div class="p- mx-auto" id='card'>
-                        <div class="h-96 bg-opacity-75 px-8 pt-16 pb-24 overflow-hidden relative" id='cards1'>
+                <div class="container flex sm:px-52 py-24">
+                    <div class="flex flex-wrap px- -m-4 mx-auto">
+                    <div class="sm:w-96 sm:mx-5" id='card'>
+                        <div class="h-96 bg-opacity-75 px-8 pt-10 pb-24 overflow-hidden relative" id='cards1'>
                             <h2 class="tracking-widest text-xs title-font text-black font-semibold mb-5">Stem</h2>
-                            <h1 class="title-font sm:text-2xl text-xl text-center font-bold text-black mb-3">How Upbringing
+                            <h1 class="title-font sm:text-2xl text-xl text-center font-bold text-black mb-16">How Upbringing
                                 changed the parent’s
                                 approach</h1>
                             <p class="leading-relaxed text-center text-black mb-3">explore how a person's upbringing can impact their approach to parenting. We'll discuss how parents' own experiences as children can influence their beliefs, attitudes, and behaviors as parents. </p>
@@ -41,16 +41,17 @@ function Cards() {
                     </div>
 
 
-                    <div class="p- pt-24 mx-auto" id='card'>
-                        <div class="h-96 bg-opacity-75 px-8 pt-16 pb-24 overflow-hidden relative" id='cards1'>
+                    <div class="sm:w-96 pt-24 sm:mx-5" id='card'>
+                        <div class="h-96 bg-opacity-75 px-8 pt-10 pb-24 overflow-hidden relative" id='cards1'>
                             <h2 class="tracking-widest text-xs title-font text-black font-semibold mb-5">Tips</h2>
-                            <h1 class="title-font sm:text-2xl text-center text-xl font-bold text-black mb-3">Enhance the Social
+                            <h1 class="title-font sm:text-2xl text-center text-xl font-bold text-black mb-16">Enhance the Social
                                 Interaction in Kids </h1>
                             <p class="leading-relaxed text-center text-black mb-3">explore how a person's upbringing can impact their approach to parenting. We'll discuss how parents' own experiences as children can influence their beliefs, attitudes, and behaviors as parents. </p>
                         </div>
-                        {/* </div> */}
+                        </div>
                     </div>
                 </div>
+                
                 
             </section>
         </div>
