4018622d09b36d0fe26aff6778ff99b98a62f703
Naman <namanb454@gmail.com>
2023-05-30 19:27:47 +0530

    new commit

--- src/constants/index.js
+++ src/constants/index.js
@@ -18,7 +18,7 @@ export const exploreWorlds = [
   {
     id: 'world-3',
     imgUrl: '/planet-03.png',
-    img: 'images/kid1.png',
+    img: 'images/kid3.png',
     title: 'The Ethics',
     desc: 'Brain develops fastest between the age of 2-8 years, and by providing the right Toys 7 resources that develops their creative & Critical thinking, and making them strong on STEM subjects, we shape their Mind.',
     prompt: 'Read more'
