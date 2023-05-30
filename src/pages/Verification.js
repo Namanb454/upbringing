80d462269f21cf95ab04e846e971a6aafe4637d7
Naman <namanb454@gmail.com>
2023-05-30 22:27:49 +0530

    new commit

--- src/pages/Verification.js
+++ src/pages/Verification.js
@@ -9,8 +9,8 @@ function Verfication() {
             
             <div className='lg:h-[10px] my-0 '>
 
-                <img className='lg:relative md:relative lg:w-[100%] lg:h-[102vh] md:h-[60vh] h-0 ' src='images/SignUpbg.png' alt='img' id='backgroundimg' />
-                <div className='md:mx-[7%]'>
+            <img className='lg:relative md:relative lg:w-[100%] lg:h-[105vh] md:h-[60vh] h-0 ' src='images/SignUpbg.png' alt='img' id='backgroundimg' />
+                <div className='md:mx-auto'>
                     <VerificationCard />
                 </div>
             </div>
